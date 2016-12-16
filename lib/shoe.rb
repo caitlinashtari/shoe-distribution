8853f843b264fcc44a850f94bbc0f70a08977e15
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-16 08:56:24 -0800

    add missing private capitalize method to store and shoe classes.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,4 +2,10 @@ class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:name, {:presence => true, :uniqueness => {case_sensitive: false}})
   before_save(:capitalize)
+
+  private
+
+  define_method(:capitalize) do
+    self.name=(name.titlecase)
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,10 @@ class Store < ActiveRecord::Base
   has_and_belongs_to_many(:shoes)
   validates(:name, {:presence => true, :uniqueness => {case_sensitive: false}})
   before_save(:capitalize)
+
+  private
+
+  define_method(:capitalize) do
+    self.name=(name.titlecase)
+  end
 end
