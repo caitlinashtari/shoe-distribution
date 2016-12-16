e17eb99514f80791ade2e90aed808e9dcc529e4a
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-16 11:57:53 -0800

    retroactively test the ability to update and delete a store in store_pages_spec.

--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -15,4 +15,26 @@ describe('store functionality through application', {:type => :feature}) do
     click_link('Baby Gap')
     expect(page).to have_content('There are no shoes at this store!')
   end
+
+  it('allows a user edit a store') do
+    visit('/')
+    fill_in('store_name', :with => 'Baby Gap')
+    click_button('Add Store')
+    click_link('Baby Gap')
+    click_link("Edit Baby Gap's Info")
+    fill_in("store_name", :with => "Gabby Bap")
+    click_button('Update')
+    expect(page).to have_content('Gabby Bap')
+  end
+
+  it('allows a user delete a store') do
+    visit('/')
+    fill_in('store_name', :with => 'Baby Gap')
+    click_button('Add Store')
+    click_link('Baby Gap')
+    click_link("Edit Baby Gap's Info")
+    click_button('Delete Store')
+    expect(page).to have_content('There are no stores yet!')
+  end
+
 end
