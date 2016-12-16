1cea4a57dcee7d089ba75a89729cdbec4c6d5006
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-16 11:03:41 -0800

    create index page with store and shoe forms and routes for both forms.

--- app.rb
+++ app.rb
@@ -1,8 +1,35 @@
+require('sinatra/activerecord')
 require("bundler/setup")
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 Bundler.require(:default)
 
 
 get('/') do
+  @stores = Store.all()
   erb(:index)
+end
+
+post('/stores') do
+  name = params.fetch('store_name')
+  @store = Store.create({:name => name})
+  if @store.save
+    @stores = Store.all
+    erb(:index)
+  else
+    erb(:error)
+  end
+end
+
+post('/shoes') do
+  name = params.fetch('shoe_name')
+  store_id = params.fetch('store_id').to_i
+  @store = Store.find(store_id)
+  @shoe = @store.shoes.create({:name => name})
+  if @shoe.save
+    @shoes= Shoe.all
+    @stores = Store.all
+    erb(:index)
+  else
+    erb(:error)
+  end
 end
