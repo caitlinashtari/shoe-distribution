65171b81de61a7d4d4aeb01eb9b6f767b7d324ab
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-16 11:51:00 -0800

    retroactively add integration spec for viewing an individual stores details/shoes.

--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -1,10 +1,18 @@
 require('spec_helper')
 
 describe('store functionality through application', {:type => :feature}) do
-  it('allows a user to add a new store') do
+  it('allows a user to add a new store and view all stores') do
     visit('/')
     fill_in('store_name', :with => 'Baby Gap')
     click_button('Add Store')
     expect(page).to have_content('Baby Gap')
+  end
+
+  it('allows a user to view details for a single store') do
+    visit('/')
+    fill_in('store_name', :with => 'Baby Gap')
+    click_button('Add Store')
+    click_link('Baby Gap')
+    expect(page).to have_content('There are no shoes at this store!')
   end
 end
