90f4075006a408c64f59969c62ae52daa1c85ab6
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-12-16 09:49:29 -0800

    add passing specs for testing private capitalize method on store and shoe classes.

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Shoe) do
   it { should have_and_belong_to_many(:stores) }
+
+  it('converts name to uppercase') do
+    store = Shoe.create({:name => "star rockets"})
+    expect(store.name).to(eq('Star Rockets'))
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Store) do
     it { should have_and_belong_to_many(:shoes) }
+
+    it('converts name to uppercase') do
+      store = Store.create({:name => "purple dinosaur"})
+      expect(store.name).to(eq('Purple Dinosaur'))
+    end
 end
